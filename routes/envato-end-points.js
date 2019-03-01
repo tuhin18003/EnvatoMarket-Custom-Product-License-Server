d839f6da5f5f372082c529194cff6e5f3e5050dd
Tuhin <tuhin@codesolz.net>
2019-03-02 01:42:46 +0600

    v1.0.0

--- routes/envato-end-points.js
+++ routes/envato-end-points.js
@@ -2,7 +2,7 @@ const errors = require( 'restify-errors');
 var Query = require('../models/envato-api-call.js');
 
 module.exports = server => {
-    server.get( '/licence-verify/:purchase_code/:website_url/:client_email', ( req, res, next ) => {
+    server.get( '/envato-licence-verify/:purchase_code/:website_url/:client_email', ( req, res, next ) => {
         try{
             Query.check_valid_purchase_item_1( req.params.purchase_code, req.params.website_url, req.params.client_email, function( result ) {
                 res.send( result ); 
